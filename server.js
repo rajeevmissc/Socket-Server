82591f9123e49f4df8abb6f0d05a8355cfcadba4
rajeevmissc <rajeevm@issc.co.in>
2025-11-27 20:37:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -524,7 +524,7 @@ app.set('busyProviders', busyProviders);
 app.set('trust proxy', 1);
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "https://videocall-theta-pearl.vercel.app",
+  origin: process.env.FRONTEND_URL || "https://www.getcompanion.in",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
